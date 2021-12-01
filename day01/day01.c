dd77c4ec1994e004c704a94aebb46ce739b982ae
nyegomir <njegomir15@gmail.com>
2021-12-01 18:21:16 +0100

    day 1 part 1

---
+++ day01/day01.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main(void) {
+
+	FILE *fp = fopen("day01_input", "r");
+
+	int incs = 0;	
+
+	int measure, tmp_measure;
+	fscanf(fp, "%i", &measure); // first measure
+
+	while (fscanf(fp, "%i", &tmp_measure) == 1)
+	{
+		if (tmp_measure > measure)
+			incs++;
+
+		measure = tmp_measure;
+	}
+	
+	printf("Increases = %i\n", incs);
+
+    fclose(fp);
+}
