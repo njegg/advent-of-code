d3207d54a00b3db943a27bc414b8d6531de142fd
nyegomir <njegomir15@gmail.com>
2021-12-03 20:02:54 +0100

    day 3 part 1 completed

---
+++ day03/d03_p01.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SHORT_INPUT "d03_short_input"
+#define INPUT	    "d03_input"
+
+
+int main(int argc) {
+
+	FILE *fp = fopen(argc > 1 ? INPUT : SHORT_INPUT, "r");
+
+	if (fp == NULL) {
+		printf("cant read file");
+		return 1;
+	}
+
+
+	int bin_len = argc > 1 ? 12 : 5;
+
+	char *sum = calloc(bin_len, sizeof(int)); // allocates and zeros
+	int digit;
+
+	while(!feof(fp)) {
+		for (int i = 0; i < bin_len; i++) {
+			fscanf(fp, "%1i", &digit);	
+			sum[i] += digit == 1 ? 1 : -1;
+		}
+	}
+
+
+	int gamma = 0, epsilon = 0;
+
+	int base = 1;
+	for (int i = bin_len - 1; i >= 0; i--) {
+		gamma   += (sum[i] >= 0) * base;
+		epsilon += (sum[i] <  0) * base;
+
+		base *= 2;
+	}
+	
+	printf("gamma = %i, epsilon = %i\n", gamma, epsilon);
+	printf("power consumption = %i", (gamma * epsilon));
+
+	fclose(fp);
+	free(sum);
+}
