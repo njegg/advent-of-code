6a65e8be0add8f55e065ad90fa948c812b96766c
nyegomir <njegomir15@gmail.com>
2021-12-10 21:11:11 +0100

    day 10 completed

--- day10/d10_p02.c
+++ day10/d10_p02.c
@@ -6,10 +6,11 @@
 #define INPUT	    "d10_input"
 
 void reset_stack();
+int middle(int len, long* scores);
 
 char obracks[4] = {'(', '[', '{', '<'};
 char cbracks[4] = {')', ']', '}', '>'};
-int  points [4] = {3, 57, 1197, 25137}; 
+int  points [4] = { 1,   2,   3,   4 }; 
 
 char *stack;
 char *sp;
@@ -25,11 +26,23 @@ int main(int ac) {
 		printf("cant read file\n");
         return 1;
 	}
-	
+
+	int lines = 1;
+	while (1) {
+		char c = fgetc(fp);
+		if (c == '\n') lines++;
+		if (c == EOF) break;
+	}
+
+	long scores[lines];
+	int index = 0;
+
+	rewind(fp);
+
 	reset_stack();
 
 	// go through all lines
-	int score = 0, corrupted;
+	int corrupted;
 	char line[200];
 	while (fgets(line, 200, fp) != NULL) {
 		corrupted = 0;
@@ -46,20 +59,39 @@ int main(int ac) {
 
 				if (c == cbracks[i]) {
 					char o = pop();
-					
-					if (o != obracks[i]) {
+					if (o != obracks[i])
 						corrupted = 1;
-						score += points[i];
-					}
 				}
 			}
 
 			i++;
 		}
+
+		if (!corrupted) {
+			long line_score = 0;
+
+			while (1) {
+				char o = pop();
+				if (o == 'E') break;
+
+				for (int i = 0; i < 4; i++) {
+					if (o == obracks[i]) {
+						char c_match = cbracks[i]; 
+						line_score *= 5;
+						line_score += points[i];
+					}
+				}
+
+			}
+
+			scores[index++] = line_score;
+		}
 		reset_stack();
 	}
 
+	int score = middle(index, scores);
 	printf(" score = %i\n", score);
+
 	return 0;
 }
 
@@ -80,4 +112,19 @@ void reset_stack() {
 	stack = malloc(200 * sizeof(char));
 	stack[0] = 'E';
 	sp = stack + 1;
+}
+
+int middle(int len, long* scores) {
+	int i, j;
+	for (i = 0; i < len-1; i++) {
+		for (j = 0; j < len-i-1; j++) {
+			if (scores[j] > scores[j+1]) {
+				long temp = scores[j];
+				scores[j] = scores[j+1];
+				scores[j+1] = temp;
+			}
+		}
+	}
+
+	return scores[len/2];
 }
