d26843500bb7e88c1294e94ec4fb0f66d9935075
Nikola Njegomir <nikola.njegomir@vegait.rs>
2023-12-04 17:25:25 +0100

    day 4 part 1

---
+++ 2023-#/Solutions/D04P01.cs
@@ -0,0 +1,25 @@
+using System.Data;
+
+namespace AoC_2023.Solutions;
+
+// --- --- Day 4: Scratchcards - Part 1 --- //
+
+public class D04P01 : SolutionBase
+{
+    public override string ExampleAnswer => "142";
+
+    public override string Solve(string inputPath)
+    {
+        return File.ReadLines(inputPath)
+            .Select(l => l.Split(':')[1])
+            .Select(l => l
+                .Split(" | ")
+                .Select(s => s.Split(' ').Where(s => s != "").ToHashSet())
+                .ToArray())
+            .Select(s => s[1].Intersect(s[0]).Count())
+            .Where(x => x > 0)
+            .Select(x => 1 << (x - 1))
+            .Sum()
+            .ToString();
+    }
+}
