86745ad8b13b086c2a24837aeafb45a997f97508
njegg <njegomir15@gmail.com>
2024-12-03 14:49:27 +0100

    more regegs

--- 2024-cs/Solutions/Day03.cs
+++ 2024-cs/Solutions/Day03.cs
@@ -9,12 +9,10 @@ namespace _2024_cs.Solutions;
 
 public partial record Day03() : Solver(AnswerOne: "174561379", AnswerTwo: "106921067")
 {
-    private const string Mul = "mul(";
-    private const string Do = "do()";
-    private const string Dont = "don't()";
-    
     [GeneratedRegex(@"(?<mul>mul\((?<x>\d{1,3}),(?<y>\d{1,3})\))")]
     private static partial Regex MulRegex();
+    
+    private static int MulXY(Match m) => int.Parse(m.Groups["x"].ToString()) * int.Parse(m.Groups["y"].ToString());
     
     public override string PartOne(IEnumerable<string> input)
     {
@@ -22,83 +20,30 @@ public partial record Day03() : Solver(AnswerOne: "174561379", AnswerTwo: "10692
 
         return MulRegex()
             .Matches(code)
-            .Select(m => int.Parse(m.Groups["x"].ToString()) * int.Parse(m.Groups["y"].ToString()))
+            .Select(MulXY)
             .Sum()
             .ToString();
     }
     
+    [GeneratedRegex(@"((?<mul>mul\((?<x>\d{1,3}),(?<y>\d{1,3})\))|(?<do>do\(\))|(?<dont>don't\(\)))")]
+    private static partial Regex MulRegex2();
+    
     public override string PartTwo(IEnumerable<string> input)
     {
         var code = input.Aggregate(seed: new StringBuilder(), (sb, s) => sb.Append(s)).ToString();
-        var mulator = 0;
 
-        while (code != "")
-        {
-            var dontIndex = code.IndexOf(Dont);
-            var mulIndex = code.IndexOf(Mul);
-
-            if (mulIndex == -1) break;
-            
-            var dontCloserThanMul = dontIndex != -1 && dontIndex < mulIndex;
-
-            var (codeSlice, mul) = dontCloserThanMul
-                ? JumpToDo(code[dontIndex..])
-                : TryReadNumbers(code[(mulIndex + 4)..]);
-                
-            code = codeSlice;
-            mulator += mul;
-        }
-
-        return mulator.ToString();
-    }
-
-    private static (string codeSlice, int mul) JumpToDo(string code)
-    {
-        var doIndex = code.IndexOf(Do);
-        var codeSlice = doIndex != -1 ? code[(doIndex + 3)..] : string.Empty;
-
-        return (codeSlice, 0);
-    }
-
-    private static (string codeSlice, int mul) TryReadNumbers(string code)
-    {
-        var mulator = 0;
-        var digitBuffer = "";
-        var commaEncountered = false;
-
-        for (var i = 0; i < code.Length; i++)
-        {
-            var c = code[i];
-
-            switch (c)
-            {
-                case var _ when char.IsDigit(c): 
+        return MulRegex2().Matches(code)
+            .Aggregate(
+                seed: (Enabled: true, Sum: 0),
+                (acc, m) => m.ToString() switch
                 {
-                    if (digitBuffer.Length == 3) return (code[i..], 0);
-                    
-                    digitBuffer += c;
-                    break;
-                }
-                
-                case ',' when int.TryParse(digitBuffer, out var x):
-                {
-                    if (commaEncountered) return (code[i..], 0);
-                
-                    commaEncountered = true;
-                    mulator = x;
-                    digitBuffer = "";
-                    break;
-                }
-                
-                case ')' when commaEncountered && int.TryParse(digitBuffer, out var y):
-                    return (code[i..], mulator * y);
-                
-                default:
-                    return (code[i..], 0);
-            }
-        }
-
-        return (string.Empty, 0);
+                    "do()" => (true, acc.Sum),
+                    "don't()" => (false, acc.Sum),
+                    _ when acc.Enabled => (true, acc.Sum + MulXY(m)),
+                    _ => acc
+                })
+            .Sum
+            .ToString();
     }
 
     protected override List<(string Expected, string Input)> PartOneExamples => [
